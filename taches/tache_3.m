3d54e214f33f364aa93a4b168edbf100f5f6a34f
ImadABID <imad.abied@gmail.com>
2021-09-28 21:46:02 +0200

    using modulate & demodulate functions and printing a message

--- taches/tache_3.m
+++ taches/tache_3.m
@@ -9,7 +9,7 @@ addpath("../src/PHY");
 Fse = 20;
 Nb = 88;
 ctrl = 24;
-middle = floor(Fse/2);
+sigma = 0.8;
 Pg = [1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 0 0 0 0 0 0 1 0 0 1]; %generator
 packet = randi([0 1], Nb, 1);
 
@@ -18,36 +18,16 @@ CRCGen = crc.generator(Pg);
 CRCDet = crc.detector(Pg);
 EncPacket = generate(CRCGen, packet); %ajout des bits de contrôle
 
-%%modulation PPM
-sigma = 1;
 sl = modulatePPM(EncPacket', Fse);
-%nl = normrnd(0,sigma, [1, length(sl)]);
-yl = sl;
-%yl(3) = ~yl(3);  
-%yl(4) = ~yl(4);  
-%yl(111) = ~yl(111);  
-
-%convolution
-p_ada = [0.5 * ones(1,middle), -0.5 * ones(1,Fse-middle)];
-vl = conv(yl, p_ada);
-
-%echantillonnage
-vm = zeros(1, Nb + ctrl);
-for k=1:(Nb + ctrl)
-    vm(k) = vl(Fse*k);
-end
-
-%decision
-b_estim = zeros(1, Nb + ctrl);
-for k=1:(Nb + ctrl)
-    if(vm(k)>0)
-        b_estim(k) = 0;
-    else
-        b_estim(k) = 1;
-    end
-end
+nl = normrnd(0, sigma, size(sl));
+yl = sl + nl;
+packet_estim = demodulatePPM(yl, Fse);
 
 %%décodeur CRC
-[DecPacket, errors] = detect(CRCDet,b_estim');
+[DecPacket, errors] = detect(CRCDet,packet_estim');
 
-
+if errors
+    fprintf("Le message est intègre\n");
+else
+    fprintf("Le message n'est pas intègre\n");
+end
