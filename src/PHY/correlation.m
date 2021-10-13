d54ecd70e1d959095ddd67317cb46f4f9564040d
Wiam <Wiamamine72@gmail.com>
2021-10-13 09:40:54 +0200

    calcul de la correlation

--- src/PHY/correlation.m
+++ src/PHY/correlation.m
@@ -1,9 +1,9 @@
-function cor = correlation(yl, sp, delta_t)
+function cor = correlation(rl, sp, delta_t)
         sp_len = length(sp);
         
         sp_square_integral = sum(abs(sp).^2);
-        yl_square_integral = sum(abs(yl(delta_t:sp_len+delta_t-1)).^2);
-        yl_sp_integral = sum(yl(delta_t:sp_len+delta_t-1).*sp);  
+        rl_square_integral = sum(abs(rl(delta_t:sp_len+delta_t-1)).^2);
+        rl_sp_integral = sum(rl(delta_t:sp_len+delta_t-1).*sp);  
         
-        cor= yl_sp_integral/(sqrt(sp_square_integral)*sqrt(yl_square_integral));
+        cor= rl_sp_integral/(sqrt(sp_square_integral)*sqrt(rl_square_integral));
 end
