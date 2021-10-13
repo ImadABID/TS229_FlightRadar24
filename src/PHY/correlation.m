c0b532f9fe3a248a4bf4dfeff400da823977d0fb
Wiam <Wiamamine72@gmail.com>
2021-10-13 10:03:12 +0200

    correlation avec seuil

--- src/PHY/correlation.m
+++ src/PHY/correlation.m
@@ -1,9 +1,15 @@
-function cor = correlation(rl, sp, delta_t)
+function thresholded = correlation(rl, sp, delta_t, seuil)
         sp_len = length(sp);
         
         sp_square_integral = sum(abs(sp).^2);
         rl_square_integral = sum(abs(rl(delta_t:sp_len+delta_t-1)).^2);
         rl_sp_integral = sum(rl(delta_t:sp_len+delta_t-1).*sp);  
         
-        cor= rl_sp_integral/(sqrt(sp_square_integral)*sqrt(rl_square_integral));
+        cor = rl_sp_integral/(sqrt(sp_square_integral)*sqrt(rl_square_integral));
+        
+        if cor <= seuil
+            thresholded = 1;
+        else
+            thresholded = 0;
+        end           
 end
